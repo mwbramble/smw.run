c9bdc6f8c7b6792ba8a8bd6c9725530b2ae67ad7
Michael Bramble <mwbramble@gmail.com>
2025-12-20 13:10:51 -0500

    update react and configure github pages deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './Components/App/App';
 import './fonts/lmsans10-regular.otf';
 import './index.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BrowserRouter><App /></BrowserRouter>);
