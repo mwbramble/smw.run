430fa0b3a85c81382b93e2e20c8d9b1ea6330387
Michael Bramble <mwbramble@gmail.com>
2025-12-31 12:39:45 -0500

    added wiki banner and fixed the h1 in nav. it was showing text-shadow on hover and i didn't want that

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Banner from '../../Images/wiki_banner.png';
 import './home.css';
 
 export default class Home extends Component{
@@ -9,11 +10,11 @@ export default class Home extends Component{
     return (
       <section className='main-container'>
         <h1 id='page-title'>Welome to the home of all things SMW Speedrunning!</h1>
+        <img src={Banner} alt="SMW Wiki Banner" />
         <p>Need to learn some strats? Be sure to check out the <b><Link to={'/levels'}>level pages</Link></b>.</p>
         <p>Don't know what category to run? Visit the <b><Link to={'/categories'}>categories section</Link></b> to learn more about them.</p>
         <p>Need some extra help learning a trick? Check out the <b><Link to={'/tutorials'}>tutorials page</Link></b>.</p>
         <p>Did you hear an SMW runner say something and you didn't know what they were talking about? Take a look at the <b><Link to={'/glossary'}>glossary</Link></b>.</p>
-        {/* <p>Looking for some SMW runners to watch? Browse through the <b><Link to={'/runners'}>runner index</Link></b>.</p> */}
         <p>Need a link to the Practice ROM? Find that and much more on the <b><Link to={'/resources'}>resources page</Link></b>.</p>
       </section>
     )
